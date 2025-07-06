cba92fbc6371fd1f142d52117b0f61e9bbaa4757
N0tZetsu <emamainn@gmail.com>
2025-07-06 18:22:09 -0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1751836889362

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_72c1443c._.js",
+  "static/chunks/src_58856811._.js"
+],
+    source: "dynamic"
+});
